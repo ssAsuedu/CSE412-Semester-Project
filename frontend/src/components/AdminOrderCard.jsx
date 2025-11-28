f5a78e9692cba49aa8ee9ae4bd071be9d2c8d282
ghibli-codes <grujicic.tamara04@gmail.com>
2025-11-27 21:54:47 -0700

    Admin card and order card styling
    Updated the styling of the borders and buttons to be more interactive and visually appealing. Animation cues that the status of an order is pending, which makes it easier to keep track of in the admin and order menus.

--- frontend/src/components/AdminOrderCard.jsx
+++ frontend/src/components/AdminOrderCard.jsx
@@ -3,12 +3,12 @@ import "../styles/AdminOrderCard.css";
 import { updateOrderStatus } from "../api.js"
 
 export const AdminOrderCard = ({ order, onStatusUpdate }) => {
-    const formattedDate = order.orderdate
+  const formattedDate = order.orderdate
     ? new Date(order.orderdate).toLocaleDateString('en-US', {
-        year: '2-digit',
-        month: '2-digit',
-        day: '2-digit'
-      })
+      year: '2-digit',
+      month: '2-digit',
+      day: '2-digit'
+    })
     : '';
   const handleMarkComplete = async () => {
     try {
@@ -20,14 +20,22 @@ export const AdminOrderCard = ({ order, onStatusUpdate }) => {
       console.error('Error updating order status:', error);
     }
   };
-
+  var cardClass;
+  var statusClass;
+  if (order.status.toLowerCase() === "complete") {
+    cardClass = "complete card"
+    statusClass = "complete";
+  }
+  else {
+    statusClass = "pending";
+  }
   return (
-    <div className="order-card">
+    <div className={`order-card ${cardClass}`}>
       <h3>Order ID: {order.orderid}</h3>
       <p>User ID: {order.userid}</p>
       <p>Total Price: ${order.totalprice.toFixed(2)}</p>
       <p>Order Date: {formattedDate}</p>
-      <p>Status: {order.status}</p>
+      <p>Status: <span className={`status ${statusClass}`}>{order.status}</span></p>
       {order.status === 'Pending' && (
         <button className="update-status-button" onClick={handleMarkComplete}>Mark Complete</button>
       )}
